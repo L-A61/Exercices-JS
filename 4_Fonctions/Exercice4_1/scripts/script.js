ae3cc9a82acaeb2c9966cab9f0af7d5813525f5a
Axel <schoolandaxel61@gmail.com>
2024-11-07 11:59:43 +0100

    Thursday Morning Progress

--- 4_Fonctions/Exercice4_1/scripts/script.js
+++ 4_Fonctions/Exercice4_1/scripts/script.js
@@ -3,8 +3,15 @@
 function produit(_x, _y) {
     var _cube = _x ** 3;
     var _produit = _x * _y;
-    return alert("Le cube de " + _x + " est égal à " + _cube), 
-    alert("Le produit de " + _x + " x " + _y + " est égal à " + _produit);
+    return document.write("<p>Le cube de " + _x + " est " +_cube + " </p>"),
+    document.write("<p>Le produit de " + _x + " x " + _y + " est égal à " + _produit + " </p>")
+}
+
+function afficheImg(_img) {
+    var img = document.createElement('img');
+    img.src = _img;
+    document.getElementById('_image').appendChild(img);
 }
 
 produit(prompt("Saisir un premier nombre pour calculer son cube et son produit"), prompt("Saisir un deuxième nombre pour calculer le produit du premier chiffre"));
+afficheImg("/Exercice4_1/assets/papillon.jpg");
--- 4_Fonctions/Exercice4_4/scripts/script.js
+++ 4_Fonctions/Exercice4_4/scripts/script.js
@@ -1,9 +1,83 @@
 // Exercice 4.4: Menu
 
-// Menu 1: Exercice 3.7
+// Menu 1: Multiples
+function multiple(N, X) {
 
-// Menu 2 Exercice 3.5
+    for (let i = 1; i <= N; i++) { // For loop continue jusqu'à ce que i atteint la valeur saisis de N
+        let NX = i * X; // NX contient la multiplication de Multiplicande et Multiplicateur ensemble 
+        console.log(i + " x " + X + " = " + NX); // multiplicande " x " Multiplicateur " = " NX
+}
+}
 
-// Menu 3: Exercice 3.8
+// Menu 2 Somme et moyenne
+function sumAverage() {
+    let nbCount = 0; // variable global pour le nombre de chiffre
+    let nbSum = 0; // variable global pour la somme
+    let nbInput; // variable global pour la saisis du nombre
 
-// Menu 4: Exercice 4.3
+    while(nbInput !== 0) {
+        nbInput = parseFloat(prompt("Saisis un nombre (0 pour finir)")); // Saisis un nombre
+
+        console.log(nbInput); // Affiche dans la console la saisis
+        nbSum += nbInput; // la somme + la saisis
+        nbCount++; // Augmentation du nombre de chiffre après la saisis 
+    }
+
+    let nbAverage = nbSum / nbCount; // Calcule la moyenne, somme / le nombre de chiffre saisis
+
+    console.log("-----BREAK-----");
+
+    console.log("Somme de la série: " + nbSum); // Affiche la somme de la série selon la valeur de la variable nbSum
+    console.log("Moyenne de la série: " + nbAverage); // Affiche la somme de la série selon la valeur de la variable nbAverage
+}
+
+// Menu 3: Recherche du nombre de voyelles
+function vowelCount(word) {
+    let vowel = "aeiouyAEIOUY"; // liste de voyelle à l'attention de l'algorithme
+    let vowelCount = 0; // variable global pour la compte de voyelle détecté
+
+    for (let i = 0; i < word.length; i++) { // i est la position du caractère lu par le code et s'arrête à la fin du mot
+        let currentIndex = word.substring(i, i + 1); // Variable qui lit le mot depuis sa position i et s'arrête avant la lecture du caractère voisin
+
+        if (vowel.indexOf(currentIndex) !== -1) { // Si l'index de currentIndex trouve une voyelle dans la liste de voyelle, augmente le compte de voyelle détecté.
+            vowelCount++;
+        }
+    }
+
+    console.log("Saisis: " + word); // Mot saisis
+    console.log("Nombre de voyelles: " + vowelCount); // Compte final de voyelles détecté par l'algorithme
+}
+
+// Menu 4: Recherche du nombre des caracteres suivants
+function letterCount(_phrase, _lettre) {
+    let i_LettreCount = 0;
+    for (let i = 0; i < _phrase.length; i++) {
+        let i_CurrentIndex = _phrase.substring(i, i+1);
+
+        if (_lettre.indexOf(i_CurrentIndex) !== -1) {
+            i_LettreCount++;
+        }
+    }
+    alert("Cette phrase contient "+i_LettreCount+" fois la/les lettre(s) "+_lettre);
+}
+
+let choice = parseInt(prompt("1- Multiples\n2- Somme et moyenne\n3- Recherche du nombre de voyelles\n4- Recherche du nombre des caracteres suivants"));
+
+
+switch(choice) {
+     case 1:
+         multiple(parseInt(prompt("Saisir le multiplicande")), parseFloat(prompt("Saisir le multiplicateur")));
+         break;
+     case 2:
+         sumAverage();
+         break;
+     case 3:
+         vowelCount(prompt("Sasir un mot ou une phrase"));
+         break;
+    case 4:
+        letterCount(prompt("Saisis une phrase"), prompt("Saisis une ou plusieurs lettres"));
+        break;
+    default:
+        break;
+
+ }
